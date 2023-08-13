1f7fc728cc577b113d18f869c22d69d3e86290d0
Fra <135766845+JungleKiosk@users.noreply.github.com>
2023-08-13 12:12:53 +0200

    v-for: print into DOM some data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <script>
 
+import {carsData} from "./data/data.js"
+
+export default {
+    name: "App",
+    data() {
+        return {
+            carsData
+        }
+    }
+}
+
+
 </script>
 
 <template>
     <div class="container p-5">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <h1>Hello</h1>
+                <h1>Cars</h1>
+                <div class="cards">
+                    <div v-for="car in carsData" :key="car.id" class="card">
+
+                        <h1>{{ car.make }}</h1>
+                        <p>{{ car.price }} $</p>
+                    
+                    </div>
+                </div>
             </div>
         </div>
     </div>
