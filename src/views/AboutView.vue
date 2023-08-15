538a285576edbfc70e3244d9a0d9cbe6aa8edf87
Fra <135766845+JungleKiosk@users.noreply.github.com>
2023-08-15 12:56:07 +0200

    change code Home - About

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,23 +1,80 @@
+<script setup>
+import { ref } from "vue";
 
+const text_swich = ref("QGIS");
+const text_Key = ref(0);
 
-<script>
+const alternatives = ["QGIS", "Python", "RStudio", "Google Earth Engine", "HTML - CSS - JS", "Leaflet", "Vite + Vuejs 3", "VueLeafle, VueRouter, VueChart", "Sass", "SQL", "QGIS"];
+let currentIndex = 0;
 
+setInterval(() => {
+    currentIndex = (currentIndex + 1) % alternatives.length;
+    text_swich.value = alternatives[currentIndex];
+    text_Key.value++;
+}, 2000);
 
 </script>
 
+
 <template>
-    <div id="cont" class="container p-5">
-        <div class="row">
-            <h1>About</h1>
-            <div class="col-lg-12">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem sit tenetur sapiente debitis quam eius,
-                    enim nostrum nulla suscipit, impedit sed saepe, autem corporis. Porro eos odio nesciunt. Molestiae,
-                    commodi!</p>
+    <main>
+        <section id="about">
+            <div class="container my-5">
+                <div class="row justify-content-center align-items-center">
+
+                    <div class="col-12 col-lg-12 bg_intro rounded-4 py-5 my-2 align-content-center">
+
+                        <h1 class="txt_initial"><strong>Hi, I'm </strong><span class="txt_fra">FRA</span></h1>
+
+                        <br>
+                        <p>
+                            I have a master's degree in Planning and Management of Agro-Territorial, Forest and Landscape:
+
+                            <a href="https://corsi.unibo.it/2cycle/LandscapePlanningManagement" class="link_efct"
+                                target="_blank"><span class="txt_thin">
+                                    PROGESA</span></a>
+                            <br>
+                            The study program contributes to the pursuit of the <br>
+                            UN 2030 Agenda for
+                            <a href="https://www.un.org/sustainabledevelopment/" class="link_efct" target="_blank"><span
+                                    class="txt_thin" target="_blank">Sustainable
+                                    Development Goals.</span> </a> <br>
+                            I have a passion for the environment and for technology.
+                            <br>
+                            I have acquired a solid knowledge in GIS analysis, Remote Sensing and I broadened my skills by
+                            attending a full-stack web development course.
+                            <transition>
+                                <h1 class="py-3">My Stack: <span class="txt_swich" :key="textKey">{{ text_swich }}</span>
+                                </h1>
+                            </transition>
+                        </p>
+                    </div>
+
+
+                    <!-- <div class="col-12 col-lg-4 d-flex flex-wrap goals mx-4 col_goals_onu">
+            <div class="col-3 text-center p-2" v-for="goal in goalsArray" :key="goal.img">
+                  <a :href="goal.link" target="_blank">
+                    <img class="rounded-4 zoom-effect-goals" :src="getImagePath(goal.img)" alt="Goal Image">
+                  </a>
+                  
             </div>
-        </div>
-    </div>
+          </div> -->
+
+                </div>
+            </div>
+        </section>
+    </main>
 </template>
 
-<style scoped>
 
+<style scoped>
+img {
+    width: 500px;
+}
+
+.txt_swich {
+    font-size: larger;
+    transition: opacity 0.5s, transform 0.5s;
+}
 </style>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,79 +1,67 @@
-<script setup>
-import { ref } from "vue";
 
-const text_swich = ref("QGIS");
-const text_Key = ref(0);
+<script>
 
-const alternatives = ["QGIS", "Python", "RStudio", "Google Earth Engine", "HTML - CSS - JS", "Leaflet", "Vite + Vuejs 3", "VueLeafle, VueRouter, VueChart", "Sass", "SQL", "QGIS"];
-let currentIndex = 0;
-
-setInterval(() => {
-  currentIndex = (currentIndex + 1) % alternatives.length;
-  text_swich.value = alternatives[currentIndex];
-  text_Key.value++;
-}, 2000);
 
 </script>
 
-
 <template>
-  <main>
-    <section id="home">
-      <div class="container my-5">
-        <div class="row justify-content-center align-items-center">
-
-          <div class="col-12 col-lg-12 bg_intro rounded-4 py-5 my-2 align-content-center">
-
-            <h1 class="txt_initial"><strong>Hi, I'm </strong><span class="txt_fra">FRA</span></h1>
-
-            <br>
-            <p>
-              I have a master's degree in Planning and Management of Agro-Territorial, Forest and Landscape:
-
-              <a href="https://corsi.unibo.it/2cycle/LandscapePlanningManagement" class="link_efct" target="_blank"><span
-                  class="txt_thin">
-                  PROGESA</span></a>
-              <br>
-              The study program contributes to the pursuit of the <br>
-              UN 2030 Agenda for
-              <a href="https://www.un.org/sustainabledevelopment/" class="link_efct" target="_blank"><span
-                  class="txt_thin" target="_blank">Sustainable
-                  Development Goals.</span> </a> <br>
-              I have a passion for the environment and for technology.
-              <br>
-              I have acquired a solid knowledge in GIS analysis
-              and Remote Sensing and I broadened my skills by attending a full-stack web development course.
-              <transition>
-                <h1 class="py-3">My Stack: <span class="txt_swich" :key="textKey">{{ text_swich }}</span> </h1>
-              </transition>
-            </p>
-          </div>
-
-
-          <!-- <div class="col-12 col-lg-4 d-flex flex-wrap goals mx-4 col_goals_onu">
-            <div class="col-3 text-center p-2" v-for="goal in goalsArray" :key="goal.img">
-                  <a :href="goal.link" target="_blank">
-                    <img class="rounded-4 zoom-effect-goals" :src="getImagePath(goal.img)" alt="Goal Image">
-                  </a>
-                  
-            </div>
-          </div> -->
-
-        </div>
+  <div id="cont" class="container p-5">
+    <div class="row">
+      <h1 class="txt_about my-3">myRepo</h1>
+      <div class="col-12 col-lg-8 jumbotron my-2">
+        <p class="txt_intro_hello my-3">
+          Hello nice people!
+          <br>
+          Welcome to my web app myRepo. <br>
+          Here you will find a place to learn and share topics concerning environmental challenges, analysis
+          techniques, technological tools, curiosities and news.
+          <br>
+          🌍🔬💻🌱
+          <br>
+        </p>
       </div>
-    </section>
-  </main>
+      <div class="col-12 col-lg-4">
+        <h1>test col-4</h1>
+        <img class="gif" v-if="hide" :src="tobyfoxsleep" v-on:mouseover="toggleGif">
+        <img class="gif" v-if="showdog" v-on:mouseleave="toggleGis" :src="tobyfox">
+      </div>
+      <div class="col-12 col-lg-12">
+        <p class="txt_intro">
+          <br>
+          This project was born from my fervent desire to put my hand to technologies for the development of
+          multipage web applications and to share my passions in the field of environmental analysis and
+          monitoring, IT, coding and geospatial analysis.
+          <br>
+          Through the showcase of my projects, you will appreciate different markup and programming
+          tricks, discover how web development technologies can be used to present and share the results.
+        </p>
+      </div>
+      <div class="col-12 col-lg-12">
+        <p class="txt_intro">
+          Through my personal notes I have created some material for didactic purposes to start programming in
+          Python.
+          In the "My Colab" section, there will be a series of tutorials designed for those who are new to
+          programming.
+          Here I'll share step-by-step how to get started programming with Python, offering a resource for anyone
+          who wants to get started with this flexible and powerful programming language.
+        </p>
+      </div>
+      <div class="col-12 col-lg-12">
+        <p class="txt_intro">
+          I am excited to share my discoveries, challenges and successes with the huge community.
+          <br>
+          Through this
+          project I want to do my part in raising awareness of respect for the environment, optimization of
+          resources, continuous self-improvement and horizontal knowledge sharing.
+          <br>
+          You are all invited to participate and contribute, no limits!
+          <br> <br>
+          Thank you for being here and joining me on this exciting journey of discovering and understanding our
+          cosmos.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
-
-<style scoped>
-img {
-  width: 500px;
-}
-
-.txt_swich {
-  font-size: larger;
-  transition: opacity 0.5s, transform 0.5s;
-}
-
-</style>
+<style scoped></style>
