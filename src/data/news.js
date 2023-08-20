e4d61dc374e735e1fe0a6435af6f5699ae7d7e77
Fra <135766845+JungleKiosk@users.noreply.github.com>
2023-08-20 20:03:29 +0200

    up-to-date news

--- src/data/news.js
+++ src/data/news.js
@@ -1,36 +1,28 @@
 const news = [
     {
         id: 1,
-        title: 'Breaking News 1',
-        link: '',
-        image: 'news_image_1.jpg',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        date: '2023-08-13'
+        title: 'Global Landscapes Forum: 5 reasons to think bamboo',
+        link: 'https://www.globallandscapesforum.org/infographic/5-reasons-to-think-bamboo/',
+        image: 'news/20_08_23/bamboo_house.jpg',
+        description: 'Why is bamboo important for biodiversity? This infographic presents 5 reasons bamboo is a unique resource for humans and animals, for purposes ranging from carbon storage to providing livelihoods and more.',
+        date: 'Aug 14, 2023'
     },
     {
         id: 2,
-        title: 'Breaking News 2',
-        link: '',
-        image: 'news_image_2.jpg',
-        description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-        date: '2023-08-12'
+        title: 'UN says climate litigation has doubled in the past 6 years',
+        link: 'https://www.weforum.org/agenda/2023/08/legal-action-fighting-climate-crisis?utm_source=linkedin&utm_medium=social_scheduler&utm_term=Climate+Change&utm_content=20%2F08%2F2023+14%3A45',
+        image: 'news/20_08_23/climate_change_litigation.jpg',
+        description: 'Lawyers may seem unlikely allies in the fight against the climate emergency but, according to a new survey, they are playing an increasingly important role in holding corporations and governments accountable for failures to tackle the climate crisis.',
+        date: 'Aug 15, 2023'
     },
     {
         id: 3,
-        title: 'Breaking News 2',
-        link: '',
-        image: 'news_image_2.jpg',
-        description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-        date: '2023-08-12'
-    },
-    {
-        id: 4,
-        title: 'Breaking News 2',
-        link: '',
-        image: 'news_image_2.jpg',
-        description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-        date: '2023-08-12'
-    },
+        title: 'Marine Microplastics',
+        link: 'https://www.youtube.com/watch?v=XI0AeRUb__E',
+        image: 'news/20_08_23/esri_microplastics.png',
+        description: 'Microplastics are tiny but create BIG problems for the ocean. NOAA: National Oceanic & Atmospheric Administration affiliate Dr. Ebenezer Nyadjro of Mississippi State University explains why putting microplastics on a map is a vital starting point for saving the oceans (and ourselves) from this pervasive pollutant.',
+        date: 'Jul 07, 2023'
+    }
 ];
 
 export default news;
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,4 +1,3 @@
-
 <script>
 
 import { ref } from "vue";
@@ -7,39 +6,39 @@ import { useRouter } from "vue-router";
 import newsData from "../data/news.js";
 
 export default {
-    name: "NewsView",
-    setup() {
-        const news = ref(newsData);
-        const router = useRouter();
-        return {
-          news,
-            router,
-        };
+  name: "NewsView",
+  setup() {
+    const news = ref(newsData);
+    const router = useRouter();
+    return {
+      news,
+      router,
+    };
+  },
+  methods: {
+    getImagePath: function (name) {
+      return new URL(`../assets/img/${name}`, import.meta.url).href;
     },
-    methods: {
-        getImagePath: function (name) {
-            return new URL(`../assets/img/${name}`, import.meta.url).href;
-        },
-    },
+  },
 };
 
 </script>
 
 
 
-
-
 <template>
-  <div class="container mt-5">
-    <div class="row">
-      <div v-for="newsItem in news" :key="newsItem.id" class="col-12 col-lg-3 mb-4">
-        <div class="card">
-          <img :src="getImagePath(newsItem.image)" class="card-img-top" alt="News Image">
+  <div class="container my-5">
+    <div class="row justify-content-center">
+      <div v-for="newsItem in news" :key="newsItem.id" class="col-12 col-lg-7 mb-4">
+        <div class="card bg_card">
+          <div class="card-header bg_card_header">
+            <h1 class="card-title">{{ newsItem.title }}</h1>
+          </div>
           <div class="card-body">
-            <h5 class="card-title">{{ newsItem.title }}</h5>
-            <a href="">Go to link {{ newsItem.link }}</a>
+            <a v-if="newsItem.link" :href="newsItem.link" target="_blank">Go to link </a>
             <p class="card-text">{{ newsItem.description }}</p>
           </div>
+          <img :src="getImagePath(newsItem.image)" class="" alt="News Image">
           <div class="card-footer">
             <small class="text-muted">{{ newsItem.date }}</small>
           </div>
@@ -52,28 +51,6 @@ export default {
   
 <style scoped>
 /* Stili personalizzati per le cards */
-.card {
-  border: none;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-}
 
-.card-title {
-  font-size: 1.25rem;
-}
-
-.card-text {
-  font-size: 1rem;
-}
-
-.card-footer {
-  background-color: #f8f9fa;
-  border-top: none;
-  text-align: right;
-  padding: 0.5rem;
-}
-
-.text-muted {
-  font-size: 0.8rem;
-}
 </style>
   
